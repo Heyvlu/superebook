9af74b26c07345d5eeb969f875ecaf17a3ad445a
Heyvlu <3059982570@qq.com>
2022-01-07 23:31:15 +0800

    modify file

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,32 @@
-import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
+import React from 'react';
+import { useState} from "react";
 import Detail from './Components/Detail/detail.jsx';
+import './App.css';
+import { Route, BrowserRouter as Router, Switch,Link} from "react-router-dom";
+import {Button} from 'antd';
 
-function App(){
-  return(
+function App() {
+  const [state,setState]=useState(1);
+  return (
     <div>
       <Router>
-      <Link to="/detail">detail</Link>
-      <Route path="/detail" component={Detail}/>
+        {state?<div>
+                <input placeholder="请输入你的目录url" className="inputBorder"/>
+                <span className="submitLink"><Link to="/detail" onClick={()=>{
+                  setState(0);
+                }}>提交</Link></span>
+              </div>:<div>
+                        <Switch>
+                          <Route exact path="/detail" component={Detail}/>
+                        </Switch>
+                        <Button className='searchBtn' onClick={()=>{
+                          setState(1);
+
+                        }}>返回搜索</Button>
+                      </div>}
       </Router>
     </div>
-  )
+  );
 }
 
 export default App;
